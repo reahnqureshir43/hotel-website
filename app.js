186a1d8d4aa015329e09a66face228c886a7414c
reahnqureshir43 <rehanqureshir43@gmail.com>
2026-06-10 10:21:10 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+// String Variable
+let hotelName = "Qureshi Hotel";
+
+document.getElementById("welcome").innerHTML =
+    "Welcome to " + hotelName;
+
+// String ka use button par
+function showMessage() {
+    let message = "Thank you for visiting Qureshi Hotel!";
+    alert(message);
+}
